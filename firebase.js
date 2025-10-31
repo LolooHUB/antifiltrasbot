d0ff31cf95a75b81f491c6761f104609cf919113
LolooHUB <lololicastro25@gmail.com>
2025-10-30 23:00:46 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,21 @@
-import admin from "firebase-admin";
-import config from "./config.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-const serviceAccount = JSON.parse(config.FIREBASE_KEY);
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEUOzOWdq0xs3qiIlX1RzB6bpKkkMLykA",
+  authDomain: "antifiltras-7116d.firebaseapp.com",
+  databaseURL: "https://antifiltras-7116d-default-rtdb.firebaseio.com",
+  projectId: "antifiltras-7116d",
+  storageBucket: "antifiltras-7116d.firebasestorage.app",
+  messagingSenderId: "795745598666",
+  appId: "1:795745598666:web:72b77816d3c2a3c17f8833",
+  measurementId: "G-8NS1EX4ZF3"
+};
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-}
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-const db = admin.firestore();
-export { db };
+export default app;
