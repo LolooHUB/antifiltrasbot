a171c8e200cc08dc2ac1cbcac5bf43aca1e6fc0a
LolooHUB <lololicastro25@gmail.com>
2026-02-12 14:19:49 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,27 +1,12 @@
-// firebase.js
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+const admin = require('firebase-admin');
 
-// Configuración de tu Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyAEUOzOWdq0xs3qiIlX1RzB6bpKkkMLykA",
-  authDomain: "antifiltras-7116d.firebaseapp.com",
-  databaseURL: "https://antifiltras-7116d-default-rtdb.firebaseio.com",
-  projectId: "antifiltras-7116d",
-  storageBucket: "antifiltras-7116d.firebasestorage.app",
-  messagingSenderId: "795745598666",
-  appId: "1:795745598666:web:72b77816d3c2a3c17f8833"
-};
+// Los secretos se configuran en GitHub Actions/Secrets
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-// Inicializar Firebase
-const app = initializeApp(firebaseConfig);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+});
 
-// Firestore
-const db = getFirestore(app);
-
-// Auth
-const auth = getAuth(app);
-
-// Exportar nombrados
-export { app, db, auth };
+const db = admin.firestore();
+module.exports = { db };
