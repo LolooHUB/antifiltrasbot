2d4f93038475643ca3728ec4dbbdca2181d74d02
LolooHUB <lololicastro25@gmail.com>
2026-02-12 15:33:43 -0300

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -9,7 +9,7 @@ for (const file of commandFiles) {
     commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
 (async () => {
     try {
