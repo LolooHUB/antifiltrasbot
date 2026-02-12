7183f3ccf095ed4026d3708855311d9b815b4762
LolooHUB <lololicastro25@gmail.com>
2026-02-12 14:36:27 -0300

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,18 +5,21 @@ const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  commands.push(command.data.toJSON());
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
-  try {
-    console.log('Actualizando comandos slash...');
-    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
-    console.log('Comandos registrados.');
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        console.log('Registrando comandos...');
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            { body: commands }
+        );
+        console.log('Comandos registrados.');
+    } catch (error) {
+        console.error(error);
+    }
 })();
